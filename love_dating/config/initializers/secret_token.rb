0614f98108e50a73a85aadc6cbed2fe3cb8bbf05
Vijay <vijaychouhan.exams@gmail.com>
2014-02-08 07:20:28 +0530

    first commit

---
+++ love_dating/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LoveDating::Application.config.secret_key_base = '52b5a9c1c9f3d5c446e3f94b4144a79a9c54dccb4fc916ec3c05d89aef1c616759f37f836c694a615ad1a4147a94a15c26edc9411553ab0a09909ebfdec62746'
---
+++ love_dating/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LoveDating::Application.config.session_store :cookie_store, key: '_love_dating_session'
